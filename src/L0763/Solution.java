edb16734236878c1ae55fb96bf3d4a07658fd1bc
xuanjia <xuanjia961121@gmail.com>
2023-04-28 17:34:42 +0800

    add 763

---
+++ src/L0763/Solution.java
@@ -0,0 +1,17 @@
+package L0763;
+
+import java.util.List;
+
+/***
+ * Medium
+ * Partition Labels
+ * @author 2008017
+ *
+ */
+public class Solution {
+	
+	public List<Integer> partitionLabels(String s) {
+
+	}
+	
+}
