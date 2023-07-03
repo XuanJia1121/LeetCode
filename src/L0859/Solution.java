77832f4366592cce998f688845cd67f49d148773
xuanjia <xuanjia961121@gmail.com>
2023-07-03 17:26:37 +0800

    add

--- src/L0859/Solution.java
+++ src/L0859/Solution.java
@@ -21,7 +21,7 @@ public class Solution {
 			for (int i = 0; i < arr.length; i++) {
 				set.add(arr[i]);
 			}
-			if (set.size() == arr.length) return false;
+			return set.size() < arr.length;
 		}
 		List<Integer> dif = new ArrayList<>();
 		for (int i = 0; i < arr.length; i++) {
